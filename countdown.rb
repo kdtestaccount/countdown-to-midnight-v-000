f20ee474b115e525e52eee2e32396bb985d60235
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-07 00:57:43 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,14 @@ def countdown(number)
   returnvalue = "HAPPY NEW YEAR!"
   returnvalue
 end
+
+
+def countdown_with_sleep(number)
+  until number == 0 
+    puts "#{number} SECOND(S)"
+    number = number - 1
+    sleep 1
+  end
+  returnvalue = "HAPPY NEW YEAR!"
+  returnvalue
+end
